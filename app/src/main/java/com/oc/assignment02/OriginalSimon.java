6665279f32c38e9ef1b1d5a5af1a4238ba9218db
Larmstrong1626 <rarmstrong7@my.apsu.edu>
2019-02-22 09:57:26 -0600

    Added Original Simon Activity java and xml

--- app/src/main/java/com/oc/assignment02/OriginalSimon.java
+++ app/src/main/java/com/oc/assignment02/OriginalSimon.java
@@ -1,3 +1,14 @@
+/*************
+ Developer: Robert Armstrong/Justin Duff
+ App: Assignment_2
+ OriginalSimon.java
+ Tested on Samsung Galaxy 5
+ 5.1"x2.85"
+ Marshmallow 6.0.1
+ API 23
+
+ **************/
+
 package com.oc.assignment02;
 
 import android.os.Bundle;
