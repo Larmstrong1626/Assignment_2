1dbffe961ec8b509eba47c82571c19c25f96c923
Larmstrong1626 <rarmstrong7@my.apsu.edu>
2019-02-22 12:52:42 -0600

    Added game buttons, formatted

--- app/src/main/java/com/oc/assignment02/OriginalSimon.java
+++ app/src/main/java/com/oc/assignment02/OriginalSimon.java
@@ -13,13 +13,21 @@ package com.oc.assignment02;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.Button;
 
 public class OriginalSimon extends Activity {
+    Button topLeft,topRight,bottomRight,bottomLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_original_simon);
+
+        topLeft.findViewById(R.id.buttontl);
+        topRight.findViewById(R.id.buttontr);
+        bottomLeft.findViewById(R.id.buttonbl);
+        bottomRight.findViewById(R.id.buttonbr);
+
     }
 
 }
