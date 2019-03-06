a83a2189357979a784e44a6f2ee5f5ce5e66f73e
justin <justind417@gmail.com>
2019-03-06 13:51:40 -0600

    -Added new sounds for new game, new high score, 4 simon buttons, and game over. created attribute for new game sound.

--- app/src/main/java/com/oc/assignment02/AboutActivity.java
+++ app/src/main/java/com/oc/assignment02/AboutActivity.java
@@ -13,20 +13,36 @@ package com.oc.assignment02;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.net.URI;
 
 public class AboutActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
     }
 
     public void back(View view)
     {
         Intent i = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(i);
+    }
+
+    public void onClick(View v){
+        if(v.getId() == R.id.newGameAttr_tv){
+
+            Uri my_link = Uri.parse("https://freesound.org/people/JustInvoke/sounds/446142/");
+            Intent intent = new Intent(Intent.ACTION_VIEW, my_link);
+            startActivity(intent);
+        }
     }
 }
 
--- app/src/main/java/com/oc/assignment02/RewindSimonActivity.java
+++ app/src/main/java/com/oc/assignment02/RewindSimonActivity.java
@@ -1,6 +1,7 @@
 package com.oc.assignment02;
 
 import android.app.Activity;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.AsyncTask;
@@ -48,6 +49,7 @@ public class RewindSimonActivity extends Activity {
     boolean Rewind_AI_Turn = true;
 
     private Rewind_Computer_player Simon;
+    FileOperations rewind_file;
 
 
     private List<Integer> rewind_AI_Choices;
@@ -60,23 +62,33 @@ public class RewindSimonActivity extends Activity {
     final Handler handler = new Handler();
     private SoundPool rewindSoundPool;
     private Set<Integer> rewindSoundsLoaded;
-    public SoundPool rewindSP = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+    public SoundPool rewindSP = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
     public int rewindTL_sound, rewindTR_sound, rewindBL_sound, rewindBR_sound;
+
+    int newGameID;
+    int highScoreID;
+    int gameOverID;
+    int redID;
+    int blueID;
+    int greenID;
+    int yellowID;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rewind);
 
-        readRewindHighScore();
+        rewind_file=new FileOperations(this,filename);
+        rewind_file.setFilename(filename);
+        rewind_score=rewind_file.getHighscore();
+       // readRewindHighScore();
 
         rewind_AI_Choices = new ArrayList<Integer>();
         rewind_User_Choices = new ArrayList<Integer>();
-        revList = new ArrayList<Integer>();
         rewind_turn = findViewById(R.id.rewindTurn_tv);
         rewind_current_score = (TextView) findViewById(R.id.currentScore_tv2);
         rewind_highScore = (TextView) findViewById(R.id.highScore_tv2);
-       rewind_highScore.setText(Integer.toString(RhighScore));
+        rewind_highScore.setText(Integer.toString(RhighScore));
 
 
 
@@ -87,24 +99,60 @@ public class RewindSimonActivity extends Activity {
         rewindBL_Btn = findViewById(R.id.button_rewind_bl);
         rewindNG_Btn = findViewById(R.id.button_rewind_ng);
 
+
+        AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
+        attrBuilder.setUsage(AudioAttributes.USAGE_GAME);
+
+        SoundPool.Builder rewindSPBuilder = new SoundPool.Builder();
+        rewindSPBuilder.setAudioAttributes(attrBuilder.build());
+        rewindSPBuilder.setMaxStreams(1);
+        rewindSoundPool = rewindSPBuilder.build();
+
+        rewindSoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener(){
+            public void onLoadComplete(SoundPool soundpool, int sampleID, int status){
+                if(status == 0){
+                    rewindSoundsLoaded.add(sampleID);
+                }else{
+
+                }
+            }
+        });
+
+       newGameID = rewindSoundPool.load(this, R.raw.new_game2, 1);
+       highScoreID = rewindSoundPool.load(this, R.raw.high_score1, 1);
+       gameOverID = rewindSoundPool.load(this, R.raw.game_over, 1);
+       greenID = rewindSoundPool.load(this, R.raw.green, 1);
+       redID = rewindSoundPool.load(this, R.raw.red, 1);
+       yellowID = rewindSoundPool.load(this, R.raw.red, 1);
+       blueID = rewindSoundPool.load(this, R.raw.blue, 1);
+
+
         rewindTL_sound = rewindSP.load(this, R.raw.one, 1);
         rewindTR_sound = rewindSP.load(this, R.raw.two, 1);
         rewindBL_sound = rewindSP.load(this, R.raw.three, 1);
         rewindBR_sound = rewindSP.load(this, R.raw.four, 1);
 
         rewindSoundsLoaded = new HashSet<Integer>();
+
         rewindNG_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startRewindGame();
 
+                if (rewindSoundsLoaded.contains(newGameID)) {
+                    rewindSoundPool.play(newGameID, 1.0f, 1.0f, 0, 0, 1.0f);
+                }
+                startRewindGame();
             }
         });
+
 
         rewindTL_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rewindSP.play(rewindTL_sound, 1, 1, 1, 0, 1f);
+                //rewindSP.play(rewindTL_sound, 1, 1, 1, 0, 1f);
+                if (rewindSoundsLoaded.contains(blueID)) {
+                    rewindSoundPool.play(blueID, 1.0f, 1.0f, 0, 0, 1.0f);
+                }
                 ButtonOpacity newopacity = new ButtonOpacity(handler, v);
                 newopacity.makeOpaque(handler, v);
                 rewind_humanMove = 1;
@@ -119,7 +167,10 @@ public class RewindSimonActivity extends Activity {
         rewindTR_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rewindSP.play(rewindTR_sound, 1, 1, 1, 0, 1f);
+                //rewindSP.play(rewindTR_sound, 1, 1, 1, 0, 1f);
+                if (rewindSoundsLoaded.contains(greenID)) {
+                    rewindSoundPool.play(greenID, 1.0f, 1.0f, 0, 0, 1.0f);
+                }
                 ButtonOpacity newopacity = new ButtonOpacity(handler, v);
                 newopacity.makeOpaque(handler, v);
                 rewind_humanMove = 2;
@@ -134,7 +185,10 @@ public class RewindSimonActivity extends Activity {
             @Override
             public void onClick(View v) {
                 //playSound(threeId);
-                rewindSP.play(rewindBL_sound, 1, 1, 1, 0, 1f);
+               // rewindSP.play(rewindBL_sound, 1, 1, 1, 0, 1f);
+                if (rewindSoundsLoaded.contains(redID)) {
+                    rewindSoundPool.play(redID, 1.0f, 1.0f, 0, 0, 1.0f);
+                }
                 ButtonOpacity rewind_newopacity = new ButtonOpacity(handler, v);
                 rewind_newopacity.makeOpaque(handler, v);
                 rewind_humanMove = 3;
@@ -149,7 +203,10 @@ public class RewindSimonActivity extends Activity {
             @Override
             public void onClick(View v) {
                 //playSound(fourId);
-                rewindSP.play(rewindBR_sound, 1, 1, 1, 0, 1f);
+               // rewindSP.play(rewindBR_sound, 1, 1, 1, 0, 1f);
+                if (rewindSoundsLoaded.contains(yellowID)) {
+                    rewindSoundPool.play(yellowID, 1.0f, 1.0f, 0, 0, 1.0f);
+                }
                 ButtonOpacity rewind_newopacity = new ButtonOpacity(handler, v);
                 rewind_newopacity.makeOpaque(handler, v);
                 rewind_humanMove = 4;
@@ -163,18 +220,20 @@ public class RewindSimonActivity extends Activity {
     public void startRewindGame() {
 
 
-        if (Simon == null) {
-            Simon = new Rewind_Computer_player();
-            Simon.execute();
-        }
-        rewindNG_Btn.setEnabled(false);
-        rewind_roundNumber = 1;
-        rewind_score = 0;
-        rewind_current_score.setText(Integer.toString(rewind_score));
-        rewind_moves = 1;
 
 
-    }
+                if (Simon == null) {
+                    Simon = new Rewind_Computer_player();
+                    Simon.execute();
+                }
+
+                rewindNG_Btn.setEnabled(false);
+                rewind_roundNumber = 1;
+                rewind_score = 0;
+                rewind_current_score.setText(Integer.toString(rewind_score));
+                rewind_moves = 1;
+            }
+
 
     public void rewindCheckChoice() {
         Log.i("-------", "----- Moves -----" + rewind_moves + "");
@@ -193,8 +252,16 @@ public class RewindSimonActivity extends Activity {
                 rewind_roundNumber++;
                 rewind_score++;
                 if(rewind_score>RhighScore){
+                    if (rewindSoundsLoaded.contains(highScoreID)) {
+                        rewindSoundPool.play(highScoreID, 1.0f, 1.0f, 0, 0, 2.5f);
+                        rewind_turn.setText("1");
+
+                    }
+
                     RhighScore=rewind_score;
-                    writeRewindHighScore();
+                    //writeRewindHighScore();
+                    rewind_file.setHighscore(rewind_score);
+                    rewind_file.writeHighScore(rewind_score,RewindSimonActivity.this);
                     rewind_highScore.setText(Integer.toString(RhighScore));
                 }
                 Simon = new Rewind_Computer_player();
@@ -203,6 +270,10 @@ public class RewindSimonActivity extends Activity {
             }
         } else {
             rewind_turn.setText("GAME OVER!");
+            if (rewindSoundsLoaded.contains(gameOverID)) {
+                rewindSoundPool.play(gameOverID, 1.0f, 1.0f, 0, 0, 1.0f);
+
+            }
             Rewind_AI_Turn = false;
             rewind_AI_Choices.clear();
             revList.clear();
@@ -214,6 +285,7 @@ public class RewindSimonActivity extends Activity {
             rewindNG_Btn.setEnabled(true);
         }
     }
+
 
     @Override
     protected void onPause() {
@@ -253,7 +325,7 @@ public class RewindSimonActivity extends Activity {
 
         @Override
         protected Void doInBackground(Void... params) {
-            int delay = 1500;
+            int delay = 2000;
             if (rewind_roundNumber < 5) {
                 delay = 1500;
             } else if (rewind_roundNumber >= 5 && rewind_roundNumber < 9) {
@@ -303,8 +375,11 @@ public class RewindSimonActivity extends Activity {
 
         public void play_sound(int soundId) {
             if (soundId == 1) {
-                soundId = rewindTL_sound;
-                rewindSP.play(soundId, 1, 1, 1, 0, 1f);
+               /* soundId = rewindTL_sound;
+                rewindSP.play(soundId, 1, 1, 1, 0, 1f);*/
+                if (rewindSoundsLoaded.contains(blueID)) {
+                    rewindSoundPool.play(blueID, 1.0f, 1.0f, 0, 0, 1.0f);
+                }
                 rewindTL_Btn.getBackground().setAlpha(51);
                 final Runnable r = new Runnable() {
                     public void run() {
@@ -317,8 +392,11 @@ public class RewindSimonActivity extends Activity {
 
             }
             if (soundId == 2) {
-                soundId = rewindTR_sound;
-                rewindSP.play(soundId, 1, 1, 1, 0, 1f);
+               // soundId = rewindTR_sound;
+               // rewindSP.play(soundId, 1, 1, 1, 0, 1f);
+                if (rewindSoundsLoaded.contains(greenID)) {
+                    rewindSoundPool.play(greenID, 1.0f, 1.0f, 0, 0, 1.0f);
+                }
                 rewindTR_Btn.getBackground().setAlpha(51);
                 final Runnable r = new Runnable() {
                     public void run() {
@@ -331,8 +409,11 @@ public class RewindSimonActivity extends Activity {
 
             }
             if (soundId == 3) {
-                soundId = rewindBL_sound;
-                rewindSP.play(soundId, 1, 1, 1, 0, 1f);
+             //   soundId = rewindBL_sound;
+               // rewindSP.play(soundId, 1, 1, 1, 0, 1f);
+                if (rewindSoundsLoaded.contains(redID)) {
+                    rewindSoundPool.play(redID, 1.0f, 1.0f, 0, 0, 1.0f);
+                }
                 rewindBL_Btn.getBackground().setAlpha(51);
                 final Runnable r = new Runnable() {
                     public void run() {
@@ -344,9 +425,11 @@ public class RewindSimonActivity extends Activity {
 
             }
             if (soundId == 4) {
-                soundId = rewindBR_sound;
-                rewindSP.play(soundId, 1, 1, 1, 0, 1f);
-
+             //   soundId = rewindBR_sound;
+              //  rewindSP.play(soundId, 1, 1, 1, 0, 1f);
+                if (rewindSoundsLoaded.contains(yellowID)) {
+                    rewindSoundPool.play(yellowID, 1.0f, 1.0f, 0, 0, 1.0f);
+                }
                 rewindBR_Btn.getBackground().setAlpha(51);
                 final Runnable r = new Runnable() {
                     public void run() {
